00d5d49bc0555139c72484f2abeb042df54a8e75
unknown <charan7728264@gmail.com>
2023-08-03 16:52:11 +0530

    first commit

--- mongoConfig.js
+++ mongoConfig.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb');
 
 const client = new MongoClient(`mongodb+srv://Charan_sai:darling_242726@cluster0.tr74xjt.mongodb.net/charanEdtech`,
-{useUnifiedTopplogy:true},
+{useUnifiedTopology:true},
 {useNewUrlParser:true},
 {connectTimeoutMS:30000},
 { keepAlive:1}
